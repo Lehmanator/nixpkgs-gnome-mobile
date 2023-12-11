f0403fa08eef34239a6756591ff67ef722833fa4
Sam Lehman <github@samlehman.dev>
2023-12-10 20:23:42 -0500

    Add input `nixpkgs` to `flake.nix` (branch: `nixpkgs-unstable`)

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "A Nix-flake to provide GNOME packages via overlay or nixosModule";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   outputs = { self, ... }: {
     overlays.default = import ./overlay.nix;
     nixosModules.gnome-mobile = import ./module.nix;

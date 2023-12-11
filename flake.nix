595e571ac955b22f0b19462ea74797c49a0ed740
Sam Lehman <github@samlehman.dev>
2023-12-10 20:21:09 -0500

    Add description attr to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 {
+  description = "A Nix-flake to provide GNOME packages via overlay or nixosModule";
   outputs = { self, ... }: {
     overlays.default = import ./overlay.nix;
     nixosModules.gnome-mobile = import ./module.nix;

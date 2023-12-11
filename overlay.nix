3858ee89b939ad179b8c6c3cdee5ce61af44051f
Sam Lehman <github@samlehman.dev>
2023-12-10 21:42:33 -0500

    Added `-devel` branch packages to overlay:
    - `gnome-shell-devel`
    - `mutter-devel`

--- overlay.nix
+++ overlay.nix
@@ -27,6 +27,17 @@ self: super: {
           --replace '<xi:include href="xml/shell-gtk-embed.xml"/>' ' '
       '';
     });
+    gnome-shell-devel = gself.gnome-shell.overrideAttrs (old: {
+      version = "unstable-2023-10-22";
+      src = super.fetchFromGitLab {
+        domain = "gitlab.gnome.org";
+        owner = "verdre";
+        repo = "mobile-shell";
+        rev = "58146d2cce4f81a2eb2de0721bb730040f0e6118";
+        hash = "sha256-b9TJlePcBi9njlly7Qx5UpfLj9pbTQUaB9rztNcnUMA=";
+        fetchSubmodules = true;
+      };
+    });
 
     mutter = gsuper.mutter.overrideAttrs (old: {
       version = "unstable-2023-09-08";
@@ -36,6 +47,16 @@ self: super: {
         repo = "mobile-mutter";
         rev = "0f08f5aba4c9b5ac34b2d5711182d50b719d838e";
         hash = "sha256-du56QMOlM7grN60eafoGTw2JGND6PK1gLrfWufihPO4=";
+      };
+    });
+    mutter-devel = gself.mutter.overrideAttrs (old: {
+      version = "unstable-2023-10-22";
+      src = super.fetchFromGitLab {
+        domain = "gitlab.gnome.org";
+        owner = "verdre";
+        repo = "mobile-mutter";
+        rev = "2c538763f7cdcd8dec29fbf602b930c60862f019";
+        hash = "sha256-D9TWF09XhqfBZyKXiPaQE+Fa+3mKG2CAdyi8nAsYa+c=";
       };
     });
   });
